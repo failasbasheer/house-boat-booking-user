5e34b94626309d1d325c5f13bfb4db839790bfd6
Failas Basheer <failasbasheer@gmail.com>
2026-01-05 20:42:24 +0400

    config: allow backend domain for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
             {
-                protocol: 'http',
-                hostname: 'localhost',
+    protocol: 'http',
+        hostname: 'localhost',
             },
-            {
-                protocol: 'http',
-                hostname: '127.0.0.1',
+{
+    protocol: 'http',
+        hostname: '127.0.0.1',
+            },
+{
+    protocol: 'https',
+        hostname: 'house-boat-booking-admin-backend.onrender.com',
             }
         ],
     },
