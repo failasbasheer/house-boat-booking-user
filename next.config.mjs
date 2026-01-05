4c9a244bcb37b9ed8a198a269810bd7f27b9843e
Failas Basheer <failasbasheer@gmail.com>
2026-01-05 20:46:09 +0400

    fix(config): correct syntax error in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        remotePatterns: [
             {
-    protocol: 'http',
-        hostname: 'localhost',
+                protocol: 'http',
+                hostname: 'localhost',
             },
-{
-    protocol: 'http',
-        hostname: '127.0.0.1',
+            {
+                protocol: 'http',
+                hostname: '127.0.0.1',
             },
-{
-    protocol: 'https',
-        hostname: 'house-boat-booking-admin-backend.onrender.com',
+            {
+                protocol: 'https',
+                hostname: 'house-boat-booking-admin-backend.onrender.com',
             }
         ],
     },
